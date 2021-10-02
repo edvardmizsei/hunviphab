8d348f2e054bc3a77afbe189a5495e3009fec3d3
edvardmizsei <34505591+edvardmizsei@users.noreply.github.com>
2021-10-02 21:05:14 +0200

    Update survreps.R

--- R/survreps.R
+++ R/survreps.R
@@ -1,7 +1,7 @@
 survreps <- function(x,sp,season,site.id,reps) {
-  x=surv
+  #x=surv
   #sp="Vipera ursinii"
-  season="season"
+  #season="season"
   #site.id="ID_SHORT"
   #reps=20
 
@@ -11,25 +11,35 @@ survreps <- function(x,sp,season,site.id,reps) {
   i <- 1
   for(i in 1:length(seasons)) {
     x.s <- x[which(x[,season]==seasons[i]),]
-
-    dat.s <- data.frame()
-    ids <- unique(x.s[,site.id])
-    j <- 1
-    for(j in 1:length(ids)) {
-      x.s.i <- x.s[which(x.s[,site.id]==ids[j]),]
-      nrow(x.s.i)
-      if(nrow(x.s.i)>=(reps+1)) {
-        x.s.i.pres <- x.s.i[which(x.s.i[,sp]>=1),]
-        x.s.i.abs <- x.s.i[which(x.s.i[,sp]==0),]
-        x.s.i.abs <- x.s.i.abs[sample.int(nrow(x.s.i.abs),reps-nrow(x.s.i.pres),replace=F),]
-        x.s.i <- rbind(x.s.i.pres,x.s.i.abs)
+    repeat{
+      dat.s <- data.frame()
+      ids <- unique(x.s[,site.id])
+      j <- 1
+      for(j in 1:length(ids)) {
+        x.s.i <- x.s[which(x.s[,site.id]==ids[j]),]
+        nrow(x.s.i)
+        if(nrow(x.s.i)>=(reps+1)) {
+          x.s.i.pres <- x.s.i[which(x.s.i[,sp]>=1),]
+          if(nrow(x.s.i.pres)>=(reps+1)) {
+            x.s.i <- x.s.i.pres[sample.int(nrow(x.s.i.pres),reps,replace=F),]
+          }
+          if(nrow(x.s.i.pres)<=(reps+1))
+            x.s.i.abs <- x.s.i[which(x.s.i[,sp]==0),]
+          x.s.i.abs <- x.s.i.abs[sample.int(nrow(x.s.i.abs),reps-nrow(x.s.i.pres),replace=F),]
+          x.s.i <- rbind(x.s.i.pres,x.s.i.abs)
+        }
+        if(nrow(x.s.i)<=reps) {
+          x.s.i <- x.s.i[sample.int(nrow(x.s.i),reps,replace=T),]
+        }
+        dat.s <- rbind(dat.s,x.s.i)
       }
-      if(nrow(x.s.i)<=reps) {
-        x.s.i <- x.s.i[sample.int(nrow(x.s.i),reps,replace=T),]
+      if(sum(dat.s[,sp])>=sum(x.s[,sp])) {
+        break
       }
-      dat.s <- rbind(dat.s,x.s.i)
     }
     dat <- rbind(dat,dat.s)
   }
   return(dat)
-} ##beépíteni egy repeat-et!! érje el az eredeti sum-ot a replikatumok
+}
+
+
